4e0a116821865e42f73f48770f93e833e63544d4
sakibhossain323 <sakibhossain323@gmail.com>
2025-08-19 21:47:47 +0600

    [FEAT] web-client: make sidebar collapsible

--- client/web-client/src/components/app-sidebar.tsx
+++ client/web-client/src/components/app-sidebar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import {
     Sidebar,
     SidebarContent,
@@ -9,21 +9,61 @@ import {
     SidebarMenuItem,
     SidebarRail,
 } from "./ui/sidebar";
+import {
+    BrainCircuitIcon,
+    CalendarIcon,
+    SquareCheckBigIcon,
+} from "lucide-react";
 
 export function AppSidebar() {
+    const items = [
+        {
+            title: "Tasks",
+            url: "/tasks",
+            icon: SquareCheckBigIcon,
+            isActive: true,
+        },
+        {
+            title: "Calendar",
+            url: "/calendar",
+            icon: CalendarIcon,
+        },
+    ];
+    const location = useLocation();
+
     return (
         <Sidebar collapsible="icon">
-            <SidebarHeader>
-                <h1>Task Scheduler</h1>
+            <SidebarHeader className="border border-b-1">
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton>
+                            <span>
+                                <BrainCircuitIcon className="h-6 w-6 text-blue-500" />
+                            </span>
+                            <h1 className="text-xl font-semibold text-blue-500">
+                                Task Scheduler
+                            </h1>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
             </SidebarHeader>
 
             <SidebarContent>
-                <SidebarMenu>
-                    <SidebarMenuItem>
-                        <SidebarMenuButton asChild>
-                            <Link to="/tasks">Tasks</Link>
-                        </SidebarMenuButton>
-                    </SidebarMenuItem>
+                <SidebarMenu className="mt-4">
+                    {items.map((item) => (
+                        <SidebarMenuItem key={item.url}>
+                            <SidebarMenuButton
+                                tooltip={item.title}
+                                isActive={item.url === location.pathname}
+                                asChild
+                            >
+                                <Link to={item.url}>
+                                    {item.icon && <item.icon />}
+                                    <span>{item.title}</span>
+                                </Link>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
                 </SidebarMenu>
             </SidebarContent>
             <SidebarFooter>
