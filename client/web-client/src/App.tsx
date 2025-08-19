ea90b89641007f0d03ca33343b45404e91751429
sakibhossain323 <sakibhossain323@gmail.com>
2025-08-19 19:14:43 +0600

    [CHORE] web-client: install react-router

--- client/web-client/src/App.tsx
+++ client/web-client/src/App.tsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Outlet } from "react-router";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <>
+            <h1>Home</h1>
+            <Outlet />
+        </>
+    );
 }
 
-export default App
+export default App;
--- client/web-client/src/main.tsx
+++ client/web-client/src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/tasks",
+                element: <h1> Tasks page</h1>,
+            },
+        ],
+    },
+]);
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
